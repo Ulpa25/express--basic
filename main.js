b62f4185f7180784537cef6082c3a9a79345a4f0
unknown <ulfash25@gmail.com>
2022-12-25 16:09:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// function jumlah (a,b){
+//   return a + b;
+// }
+
+// console.log(jumlah(1,2));
+
+// const kurang = function (a,b){
+//   return a + b;
+// } 
+// console.log(kurang(1,6));
+
+// // arrow function
+// const bagi = (a,b) => {
+//   return a / b;
+// }
+// setTimeout(() =>console.log('heloo ulfa'),1000);
+
+// setTimeout(function(){
+//   const a = "ulfa";
+//   // prompt(a+"Gileh");
+// },3000);
+
+// const nama = "Ulfa";
+// const umur = 20;
+
+// const tampilNama = (nama,umur) => "Nama : "+nama+" Umur : "+umur;
+// // export{nama, tampilNama};
+// export default tampilNama;
+
+
